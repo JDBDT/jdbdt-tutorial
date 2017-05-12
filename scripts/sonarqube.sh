517e300a126088e8b1c75d7993b3b46ac1fb15de
Eduardo Marques <eduardorbmarques@gmail.com>
2017-05-13 00:55:50 +0100

    Fixed SonarQube integration

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -1,3 +1,4 @@
-mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package sonar:sonar \
+mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent test sonar:sonar \
     -Dsonar.host.url=https://sonarqube.com \
-    -Dsonar.login=$SONARQUBE_TOKEN
+    -Dsonar.login=$SONARQUBE_TOKEN \
+    -Dsonar.organization=jdbdt
